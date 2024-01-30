b6e2f533c6df91aaa30cdfd59db6dfe0cf430bf8
Phillip khoza <phillipantonk@gmail.com>
2024-01-30 23:14:13 +0200

    first screen and second screen

--- app/src/main/java/com/phillipdev/mathapp/MainActivity.kt
+++ app/src/main/java/com/phillipdev/mathapp/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.phillipdev.mathapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var add : Button
+    lateinit var subtract : Button
+    lateinit var multiple : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        add = findViewById(R.id.btnAdd)
+        subtract = findViewById(R.id.btnSubt)
+        multiple = findViewById(R.id.btnMult)
+
+        add.setOnClickListener {
+            var intent = Intent(this@MainActivity, GameActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
